6b27af04735c629d5470e19fc263daafc908244c
dmitriydrozhzhin <drozhzhindmitriy@icloud.com>
2025-05-24 08:51:14 +0300

    add function should_see and update other pages

--- conftest.py
+++ conftest.py
@@ -1,17 +1,35 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.chrome.options import Options as ChromeOptions
 
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default='en')
+    parser.addoption("--browser_name", action="store", default="chrome",
+                     help="--browser_name should be chrome or firefox")
+@pytest.fixture
+def user_language(request):
+    return request.config.getoption("language")
 
-@pytest.fixture(scope="function")
-def browser(request):
-    user_language = request.config.getoption("language")
-    options = Options()
-    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-    print(f"Запуск браузера с установленным языком: {user_language}")
-    browser = webdriver.Chrome(options=options)
+@pytest.fixture()
+def browser(user_language,request):
+    browser_name = request.config.getoption("browser_name")
+
+
+    if browser_name == "chrome":
+        options = ChromeOptions()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options=options)
+
+    elif browser_name == "firefox":
+        options = FirefoxOptions()
+        profile = webdriver.FirefoxProfile()
+        profile.set_preference("intl.accept_languages", user_language)
+        profile.update_preferences()
+        options.profile = profile
+        browser = webdriver.Firefox(options=options)
+
+    else:
+        raise pytest.UsageError("--browser_name должен быть chrome или firefox")
     yield browser
-    print("\nquit browser..")
     browser.quit()
